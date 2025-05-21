cde48a4f591180b01aacb9ede0e3acb46f7b54d1
nane <gnane5940@gmail.com>
2025-05-21 23:29:45 +0530

    2025-05-21

--- redis.sh
+++ redis.sh
@@ -15,34 +15,35 @@ mkdir -p "$LOGS_FOLDER"
 echo "Script started executing at: $(date)" | tee -a "$LOG_FILE"
 
 VALIDATE(){
-    if [ $1 -eq 0 ]
-    then echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-    else echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
-         exit 1
-    fi  
+    if [ $1 -eq 0 ]; then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a "$LOG_FILE"
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a "$LOG_FILE"
+        exit 1
+    fi
 }
 
-dnf module disable redis -y &>>$LOG_FILE
+dnf module disable redis -y &>>"$LOG_FILE"
 VALIDATE $? "Disabling default redis"
 
-dnf enable redis:7 -y &>>$LOG_FILE
+dnf module enable redis:7 -y &>>"$LOG_FILE"
 VALIDATE $? "Enabling redis:7"
 
-dnf install redis -y &>>$LOG_FILE
+dnf install redis -y &>>"$LOG_FILE"
 VALIDATE $? "Installing redis"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g'  -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/^protected-mode/ c protected-mode no' /etc/redis/redis.conf
 VALIDATE $? "Editing /etc/redis/redis.conf file for remote connections"
 
-systemctl enable redis &>>$LOG_FILE
+systemctl enable redis &>>"$LOG_FILE"
 VALIDATE $? "Enabling redis service"
 
-systemctl start redis &>>$LOG_FILE
+systemctl start redis &>>"$LOG_FILE"
 VALIDATE $? "Starting redis service"
 
 END_TIME=$(date +%s)
 DIFF_TIME=$((END_TIME - START_TIME))
-echo "Script execution time: $((DIFF_TIME / 60)) minutes and $((DIFF_TIME % 60)) seconds" | tee -a $LOG_FILE
-echo "Script started at: $(date)" | tee -a $LOG_FILE    
-echo "Script completed at: $(date)" | tee -a $LOG_FILE
-echo -e "$G Redis installation completed successfully $N" | tee -a $LOG_FILE
+echo "Script execution time: $((DIFF_TIME / 60)) minutes and $((DIFF_TIME % 60)) seconds" | tee -a "$LOG_FILE"
+echo "Script started at: $(date)" | tee -a "$LOG_FILE"
+echo "Script completed at: $(date)" | tee -a "$LOG_FILE"
+echo -e "$G Redis installation completed successfully $N" | tee -a "$LOG_FILE"
