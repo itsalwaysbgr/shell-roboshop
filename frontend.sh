192f54962595b76d29b79ca81ca71d2011907432
nane <gnane5940@gmail.com>
2025-05-21 19:49:47 +0530

    2025-05-21

--- frontend.sh
+++ frontend.sh
@@ -6,26 +6,24 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+SCRIPT_NAME=$(basename "$0" | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
-# check the user has root priveleges or not
-if [ $USERID -ne 0 ]
-then
+# check the user has root privileges or not
+if [ $USERID -ne 0 ]; then
     echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1 #give other than 0 upto 127
+    exit 1
 else
     echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
-# validate functions takes input as exit status, what command they tried to install
+# validate function
 VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
+    if [ $1 -eq 0 ]; then
         echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
     else
         echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
@@ -42,8 +40,8 @@ VALIDATE $? "Enabling Nginx:1.24"
 dnf install nginx -y &>>$LOG_FILE
 VALIDATE $? "Installing Nginx"
 
-systemctl enable nginx  &>>$LOG_FILE
-systemctl start nginx 
+systemctl enable nginx &>>$LOG_FILE
+systemctl start nginx &>>$LOG_FILE
 VALIDATE $? "Starting Nginx"
 
 rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
@@ -52,15 +50,18 @@ VALIDATE $? "Removing default content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
 VALIDATE $? "Downloading frontend"
 
-cd /usr/share/nginx/html 
+cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>$LOG_FILE
-VALIDATE $? "unzipping frontend"
+VALIDATE $? "Unzipping frontend"
 
 rm -rf /etc/nginx/nginx.conf &>>$LOG_FILE
-VALIDATE $? "Remove default nginx conf"
+VALIDATE $? "Removing default nginx.conf"
 
-cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
-VALIDATE $? "Copying nginx.conf"
+cp "$SCRIPT_DIR/nginx.conf" /etc/nginx/nginx.conf 2>>$LOG_FILE
+VALIDATE $? "Copying custom nginx.conf"
 
-systemctl restart nginx 
-VALIDATE $? "Restarting nginx"
+# Debug info
+ls -l "$SCRIPT_DIR/nginx.conf" /etc/nginx/nginx.conf >>$LOG_FILE
+
+systemctl restart nginx &>>$LOG_FILE
+VALIDATE $? "Restarting Nginx"
